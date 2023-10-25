7ca443b61f0007c94aa38d51e7a4d12d7badbe43
Braeden Kloke <braedenkloke@gmail.com>
2023-10-25 17:00:01 -0400

    fix: fix output of non verbose sh scripts

--- scripts/sh/test2_non-verbose.sh
+++ scripts/sh/test2_non-verbose.sh
@@ -2,4 +2,4 @@
 #
 # Runs non-verbose test case 2.
 
-./out/assignment2.o "./resources/test/test_case_2.csv" 0 > output.csv
+./out/assignment2.o "./resources/test/test_case_2.csv" 0 > out/output.csv
