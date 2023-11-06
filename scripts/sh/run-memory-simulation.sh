3f1ebf49d556e3b1c0a3cf5ad506d5ecf9adeb15
Braeden Kloke <braedenkloke@gmail.com>
2023-11-06 16:50:00 -0500

    add: memory simulation results

--- scripts/sh/run-memory-simulation.sh
+++ scripts/sh/run-memory-simulation.sh
@@ -11,7 +11,9 @@ do
 		echo "Running scenario ${i} with memory schema ${j}"
 		scenario_file="./resources/memory-simulation/scenario${i}.csv"
 		memory_schema_file="./resources/schemas/memory_schema_${j}.csv"
-		./out/assignment2.o $scenario_file 0 $memory_schema_file 1 0
+		output_file="./out/memory-simulation/scenario${i}_memory${j}.csv"
+		./out/assignment2.o $scenario_file 0 $memory_schema_file 1 0 > $output_file
+		cat $output_file
 	done
 done
 
